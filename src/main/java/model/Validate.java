3e34480a41b865e6620101737bd928a19ccab277
maltebp <maltebp94@gmail.com>
2019-03-06 15:52:33 +0100

    Updated name validation - fixes #13

--- src/main/java/model/Validate.java
+++ src/main/java/model/Validate.java
@@ -5,11 +5,9 @@ public class Validate {
 
 
     public static boolean username(String username){
-        if( username.length() < 4   ||
-            username.contains(" ")  ||
-            Character.isDigit(username.charAt(0))
-        ){ return false; }
-        return true;
+        return username.length() > 1   &&
+            !username.contains(" ")  &&
+            Character.isLetter(username.charAt(0));
     }
 
 }
